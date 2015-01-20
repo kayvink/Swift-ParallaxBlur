089c709f0266dbc36a1eee9fef8f8b0f7f525dc5
kayvink <kmvink@gmail.com>
2015-01-20 16:43:33 +0000

    Initial Commit

---
+++ ParallaxBlurEffect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParallaxBlurEffect
+//
+//  Created by Kay Vink on 20/01/15.
+//  Copyright (c) 2015 Kay Vink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
